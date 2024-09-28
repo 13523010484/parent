ce589bb209807dfbf148bafebd1d0513036f2f21
jiaqh <2286592704@qq.com>
2024-09-28 12:41:24 +0800

    提交主项目

---
+++ public/register-apps.js
@@ -0,0 +1,21 @@
+document.subApps = [
+  // {
+  //   name: "childApp1",
+  //   entry: "//localhost:8088",
+  //   container: "#container-sub-app",
+  //   activeRule: "/child-app1",
+  // },
+  // {
+  //   name: "subAppReact",
+  //   entry: "//localhost:8000",
+  //   container: "#container-sub-app",
+  //   activeRule: "/sub-app-react",
+  // },
+  // 部署
+  {
+    name: "childApp1",
+    entry: "//192.168.18.171:8088",
+    container: "#container-sub-app",
+    activeRule: "/child-app1",
+  },
+];
